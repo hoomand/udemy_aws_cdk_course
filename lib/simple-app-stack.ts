2ec637c771491a7755b0fbcb043c4d5eb338e7bc
Bijan Hoomand <hoomandb@amazon.com>
2021-02-02 16:43:30 +1100

    feat: add S3 bucket & modify stack output

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,9 +1,17 @@
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'CDKTrainingBucket', {
+      encryption: BucketEncryption.S3_MANAGED,
+    })
+
+    new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
+      value: bucket.bucketName,
+      exportName: 'MySimpleAppBucketName'
+    })
   }
 }
