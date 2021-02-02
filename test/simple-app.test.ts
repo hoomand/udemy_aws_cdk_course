004c009cb263a961027309b6cbae9d57fe172873
Bijan Hoomand <hoomandb@amazon.com>
2021-02-02 17:23:46 +1100

    tst(stack): check S3 exists

--- test/simple-app.test.ts
+++ test/simple-app.test.ts
@@ -1,13 +1,12 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as SimpleApp from '../lib/simple-app-stack';
+import '@aws-cdk/assert/jest';
 
-test('Empty Stack', () => {
+test('Stack create a S3 bucket', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(stack).toHaveResource('AWS::S3::Bucket')
 });
