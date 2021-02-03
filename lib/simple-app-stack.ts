3255b6bec90d08aa7d8dd4083aa0bcf4d00c6bad
Bijan Hoomand <hoomandb@amazon.com>
2021-02-03 13:21:48 +1100

    feat: add lambda

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,17 +1,31 @@
-import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
-import * as cdk from '@aws-cdk/core';
+import { Bucket, BucketEncryption } from "@aws-cdk/aws-s3";
+import * as lambda from "@aws-cdk/aws-lambda-nodejs";
+import * as cdk from "@aws-cdk/core";
+import { Runtime } from "@aws-cdk/aws-lambda";
+import * as path from "path";
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new Bucket(this, 'CDKTrainingBucket', {
+    const bucket = new Bucket(this, "CDKTrainingBucket", {
       encryption: BucketEncryption.S3_MANAGED,
-    })
+    });
 
-    new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
+    const getPhotos = new lambda.NodejsFunction(this, "MySimpleAppLambda", {
+      runtime: Runtime.NODEJS_12_X,
+      entry: path.join(__dirname, "..", "api", "get-photos", "index.ts"),
+      handler: "getPhotos",
+    });
+
+    new cdk.CfnOutput(this, "MySimpleAppBucketNameExport", {
       value: bucket.bucketName,
-      exportName: 'MySimpleAppBucketName'
-    })
+      exportName: "MySimpleAppBucketName",
+    });
+
+    new cdk.CfnOutput(this, "MySimpleAppLambdaExport", {
+      value: getPhotos.toString(),
+      exportName: "MySimpleAppLambda",
+    });
   }
 }
