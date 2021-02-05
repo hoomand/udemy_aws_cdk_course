62bb6fbfba3a5a75e206cf270e427b7466b2c9b2
Bijan Hoomand <hoomand@gmail.com>
2021-02-05 14:37:15 +1100

    feat: upload photos to S3 & lambda list S3

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -3,15 +3,47 @@ import {
   Context,
   APIGatewayProxyResultV2,
 } from "aws-lambda";
+import { S3 } from "aws-sdk";
 
+const s3 = new S3();
+// This is passed to lambda as environment variable in the stack file
+const bucketName = process.env.PHOTO_BUCKET_NAME!;
+
+async function generateUrl(
+  object: S3.Object
+): Promise<{ filename: string; url: string }> {
+  const url = await s3.getSignedUrlPromise("getObject", {
+    Bucket: bucketName,
+    Key: object.Key!,
+    Expires: 24 * 60 * 60,
+  });
+  return {
+    filename: object.Key!,
+    url,
+  };
+}
 async function getPhotos(
   event: APIGatewayProxyEventV2,
   context: Context
 ): Promise<APIGatewayProxyResultV2> {
-  return {
-    statusCode: 200,
-    body: "Hello from lambda, it is alive!",
-  };
+  console.log(`I got the bucket name and it is ${bucketName}`);
+  try {
+    const { Contents: results } = await s3
+      .listObjects({ Bucket: bucketName })
+      .promise();
+    const photos = await Promise.all(
+      results!.map((result) => generateUrl(result))
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(photos),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.message,
+    };
+  }
 }
 
 export { getPhotos };
--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -3,6 +3,8 @@ import * as lambda from "@aws-cdk/aws-lambda-nodejs";
 import * as cdk from "@aws-cdk/core";
 import { Runtime } from "@aws-cdk/aws-lambda";
 import * as path from "path";
+import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,11 +14,32 @@ export class SimpleAppStack extends cdk.Stack {
       encryption: BucketEncryption.S3_MANAGED,
     });
 
+    new BucketDeployment(this, "MySimpleAppPhotos", {
+      sources: [Source.asset(path.join(__dirname, "..", "photos"))],
+      destinationBucket: bucket,
+    });
+
     const getPhotos = new lambda.NodejsFunction(this, "MySimpleAppLambda", {
       runtime: Runtime.NODEJS_12_X,
       entry: path.join(__dirname, "..", "api", "get-photos", "index.ts"),
       handler: "getPhotos",
+      environment: {
+        PHOTO_BUCKET_NAME: bucket.bucketName, // Passing the bucket name as env variable to our external lambda
+      },
     });
+
+    // Define bucket access policy
+    const bucketContainerPermissions = new PolicyStatement();
+    bucketContainerPermissions.addResources(bucket.bucketArn);
+    bucketContainerPermissions.addActions("s3:ListBucket");
+
+    const bucketPermissions = new PolicyStatement();
+    bucketPermissions.addResources(`${bucket.bucketArn}/*`);
+    bucketPermissions.addActions("s3:GetObject", "s3:PutObject");
+
+    // Adding bucket access permission to the lambda function
+    getPhotos.addToRolePolicy(bucketContainerPermissions);
+    getPhotos.addToRolePolicy(bucketPermissions);
 
     new cdk.CfnOutput(this, "MySimpleAppBucketNameExport", {
       value: bucket.bucketName,
